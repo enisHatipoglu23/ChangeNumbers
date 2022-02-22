221c1530f49d1357d131a2871f96d81ba02033d9
Enis Hatipoğlu <83842630+enisHatipoglu23@users.noreply.github.com>
2022-02-22 15:33:46 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    //multiply of float numbers
+    //changing nums
     public static void main(String[] args) {
 	// write your code here
     Scanner scanner = new Scanner(System.in);
