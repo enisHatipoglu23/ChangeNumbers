d212b9637554d47167acb9e3fc90be46d220b28c
Enis Hatipoğlu <enishatipoglu.2001y@gmail.com>
2022-02-22 15:27:55 +0300

    Change Numbers
    Given two numbers positions change.

---
+++ Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    //multiply of float numbers
+    public static void main(String[] args) {
+	// write your code here
+    Scanner scanner = new Scanner(System.in);
+
+        System.out.println("First Num: ");
+        double a = scanner.nextDouble();
+        System.out.println("Second Num: ");
+        double b = scanner.nextDouble();
+
+        System.out.println("-BEFORE CHANGING-");
+        System.out.println("First Num = " + a);
+        System.out.println("Second Num = " + b);
+
+        System.out.println("-AFTER CHANGING-");
+        System.out.println("First Num = " + b);
+        System.out.println("Second Num = " + a);
+
+
+
+
+
+
+    }
+}
